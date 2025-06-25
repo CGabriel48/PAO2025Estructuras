b43e066ed48b39a69b7406f1ed306a551ab7119b
CGabriel48 <cg.loorq@uea.edu.ec>
2025-06-25 18:45:33 -0500

    Practico experimental(creacion de clase registro)

--- Estructura de datos/obj/Debug/net9.0/Estructura de datos.AssemblyInfo.cs
+++ Estructura de datos/obj/Debug/net9.0/Estructura de datos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estructura de datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b47f16a91c5b6595a31e504497b877407f26eb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db97088cc9a1ec1b13a616f0ab75dedc0bb61163")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estructura de datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estructura de datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Practico experimental/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgendaTurnosClinica
+{
+    // Clase que representa un paciente (registro)
+    class Paciente
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Fecha { get; set; }
+        public string Hora { get; set; }
+        public string Especialidad { get; set; }
+
+        public Paciente(string cedula, string nombre, string fecha, string hora, string especialidad)
+        {
+            Cedula = cedula;
+            Nombre = nombre;
+            Fecha = fecha;
+            Hora = hora;
+            Especialidad = especialidad;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nombre} - {Cedula} - {Fecha} - {Hora} - {Especialidad}";
+        }
+    }
+}
