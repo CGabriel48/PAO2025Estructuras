26a83e68bac8c523e68f904b91f5446fafccfe0b
CGabriel48 <cg.loorq@uea.edu.ec>
2025-06-29 16:15:49 -0500

    Primer ejercicio

---
+++ Semana 05/Program.cs
@@ -0,0 +1,44 @@
+﻿// Ejerciocio 1 
+//Edscribir un programa que almacene las asignaturas de un curso en
+//en una lista y la muestre por panta el mensaje  Yo estudio <asignatura>,
+//donde <asignatura> es cada una de las asignaturas de la lista. 
+using System;
+using System.Collections.Generic;
+
+namespace Ejercicio1
+{
+    class Asignatura
+    {
+        public string Nombre { get; set; }
+
+        public Asignatura(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine($"Yo estudio {Nombre}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            List<Asignatura> lista = new List<Asignatura>
+            {
+                new Asignatura("Matemáticas"),
+                new Asignatura("Física"),
+                new Asignatura("Química"),
+                new Asignatura("Historia"),
+                new Asignatura("Lengua")
+            };
+
+            foreach (var asignatura in lista)
+            {
+                asignatura.Mostrar();
+            }
+        }
+    }
+}
